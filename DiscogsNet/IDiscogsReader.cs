41189a097f4a335e5fbc4021c44b735b9308759f
NickStrupat <nick.strupat@gmail.com>
2015-01-30 16:53:08 -0500

    Added IDisposable so all IDiscogsReader<T>s can be disposed through IDisposable (in a `using` construct, for example)

--- DiscogsNet/IDiscogsReader.cs
+++ DiscogsNet/IDiscogsReader.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace DiscogsNet
 {
-    public interface IDiscogsReader<T>
+    public interface IDiscogsReader<T> : IDisposable
     {
         T Read();
         IEnumerable<T> Enumerate();
